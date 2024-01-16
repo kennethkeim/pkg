4fca2e08f18fa6459e6c27c3d6a7b10e94fe4f21
Kenneth Keim <32063766+kennethkeim@users.noreply.github.com>
2024-01-15 21:18:24 -0500

    Implement mailer

--- packages/api-utils-core/README.md
+++ packages/api-utils-core/README.md
@@ -1,3 +1,14 @@
 # What is this
 
 A set of API utilities intended for my own personal/commercial use.
+
+# Mailer
+
+Module to send mail via [Brevo](https://brevo.com) via the credentials from [this page](https://app.brevo.com/settings/keys/smtp).
+
+Set the following in your environment variables.
+
+- `MAILER_USER` (Brevo credentials)
+- `MAILER_PASS` (Brevo credentials)
+- `SYS_EVENTS_SENDER` (Default email to send from)
+- `SYS_EVENTS_RECIPIENT` (Default email to send to - and set as the reply to)
--- packages/api-utils-core/src/mailer/index.ts
+++ packages/api-utils-core/src/mailer/index.ts
@@ -1,1 +1,64 @@
-export const mailer = { send: () => console.log('send')}
+import * as nodemailer from "nodemailer";
+import type SMTPTransport from "nodemailer/lib/smtp-transport";
+import * as z from "zod";
+
+export interface SendMailOptions {
+  to?: string;
+  from?: string;
+  subject: string;
+  html: string;
+}
+
+const envSchema = z.object({
+  user: z.string().email(),
+  pass: z.string().min(3),
+  sysEventsSender: z.string().email(),
+  sysEventsRecipient: z.string().email(),
+});
+
+const env = envSchema.parse({
+  user: process.env["MAILER_USER"],
+  pass: process.env["MAILER_PASS"],
+  sysEventsSender: process.env["SYS_EVENTS_SENDER"],
+  sysEventsRecipient: process.env["SYS_EVENTS_RECIPIENT"],
+});
+
+export class Mailer {
+  private transporter: nodemailer.Transporter;
+
+  /** @param appName Name of your app as it appears in email sender. */
+  constructor(appName: string) {
+    // https://app.brevo.com/settings/keys/smtp
+    // https://nodemailer.com/smtp/#tls-options
+    const smtpTransportOptions: SMTPTransport.Options = {
+      auth: { user: env.user, pass: env.pass },
+      host: "smtp-relay.brevo.com",
+      port: 587,
+      // Does not mean we're not using TLS, see nodemailer link above
+      secure: false,
+      // Require STARTTLS
+      requireTLS: true,
+    };
+
+    const defaults: SMTPTransport.Options = {
+      to: env.sysEventsRecipient,
+      from: `"${appName}" <${env.sysEventsSender}>`,
+      replyTo: env.sysEventsRecipient,
+    };
+
+    // create reusable transporter object using credentials and defaults
+    this.transporter = nodemailer.createTransport(
+      smtpTransportOptions,
+      defaults
+    );
+
+    // automatically provide plain text version of all emails
+    // htmlToText is from nodemailer-html-to-text, but we probably don't need it
+    // it's about as big as nodemailer itself
+    // this.transporter.use("compile", htmlToText());
+  }
+
+  public async send(options: SendMailOptions): Promise<void> {
+    await this.transporter.sendMail(options);
+  }
+}
