659b5df8c9fed1e9008f3a7a6819a7e0ed569250
Kenneth Keim <32063766+kennethkeim@users.noreply.github.com>
2024-01-17 23:08:32 -0500

    chore: add unit test

---
+++ packages/api-utils-core/tests/zod-error.test.ts
@@ -0,0 +1,18 @@
+import { ZodError } from "zod"
+import { getFirstZodIssue } from "../src"
+
+describe("Zod Error Utilities", () => {
+  it("Should parse zod issues correctly", () => {
+    const zodErr = new ZodError([
+      {
+        path: ["user", "firstName"],
+        message: "Required",
+        code: "invalid_literal",
+        expected: "string",
+        received: undefined,
+      },
+    ])
+
+    expect(getFirstZodIssue(zodErr)).toEqual("user.firstName: Required")
+  })
+})
