4a93f188ca1c4591fdd639285cdb833c4b3fc6fa
Kenneth Keim <32063766+kennethkeim@users.noreply.github.com>
2024-05-17 08:13:50 -0400

    Add error response utils

---
+++ packages/api-utils-core/src/errors/error-response.ts
@@ -0,0 +1,55 @@
+import { ApiError, getApiError } from "./exceptions"
+import type { Mailer } from "../mailer"
+
+export interface ErrorResponse {
+  message: string
+}
+
+export const getErrorResponse = (error: ApiError): ErrorResponse => {
+  return {
+    message: error.message,
+  }
+}
+
+export interface GenericResponse {
+  status: (status: number) => {
+    json: (json: Record<string, unknown>) => void
+  }
+}
+
+export interface GenericLogger {
+  error: (error: ApiError) => void
+}
+
+export const handleApiError = async (
+  error: unknown,
+  logger?: GenericLogger,
+  mailer?: Mailer,
+  res?: GenericResponse
+): Promise<void> => {
+  const apiError = getApiError(error)
+  logger?.error(apiError)
+
+  try {
+    const now = new Date()
+    // ISO: 2024-05-15T12:47:23.039Z
+    // Add date and hour to subject line so gmail groups emails per hour
+    const subjectDateStr = now.toISOString().slice(0, 13)
+    const cause = apiError.cause
+
+    // Can't fire and forget from serverless fn
+    await mailer?.send({
+      subject: `API Error [${apiError.status}] [${subjectDateStr}]`,
+      html: `
+          <pre>Status: ${apiError.status}</pre>
+          <pre>${apiError.stack}</pre>
+          <pre>${cause?.stack ?? "No nested error"}</pre>
+          <pre>Time: ${now.toISOString()}</pre>`,
+    })
+  } catch (err) {
+    console.log("Error sending email for error.")
+  }
+
+  // Only send response if response util is provided
+  res?.status(apiError.status).json({ message: apiError.message })
+}
