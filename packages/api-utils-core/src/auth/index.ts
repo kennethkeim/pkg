96fdbdf2527e18642402e6e021ca0a708c9cf750
Kenneth Keim <32063766+kennethkeim@users.noreply.github.com>
2024-05-18 11:45:49 -0400

    Add auth utils

---
+++ packages/api-utils-core/src/auth/index.ts
@@ -0,0 +1,25 @@
+import type { IncomingHttpHeaders } from "http"
+import { ClientError } from "../errors/exceptions"
+
+export type HttpMethod = "OPTIONS" | "GET" | "POST" | "PATCH" | "PUT" | "DELETE"
+
+/** Allow only defined methods */
+export const allowMethods = (
+  allowed: HttpMethod[],
+  actual: string | undefined
+): void => {
+  const actualMethod = (actual ?? "").toUpperCase() as HttpMethod
+  if (!allowed.includes(actualMethod)) {
+    throw new ClientError(405)
+  }
+}
+
+/** Authenticate request by checking "Authorization" header for token. */
+export const authRequest = (
+  reqHeaders: IncomingHttpHeaders,
+  token: string
+): void => {
+  if (reqHeaders["Authorization"] !== token) {
+    throw new ClientError(403)
+  }
+}
