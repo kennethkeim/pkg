7660b604a03128a01819aa98e3544621f7bbb753
Kenneth Keim <32063766+kennethkeim@users.noreply.github.com>
2024-12-09 17:22:38 -0500

    Add 429

--- packages/core/src/errors/exceptions.ts
+++ packages/core/src/errors/exceptions.ts
@@ -6,6 +6,7 @@ export type ClientErrorStatus =
   | HttpStatus.BadRequest
   | HttpStatus.MethodNotAllowed
   | HttpStatus.NotFound
+  | HttpStatus.TooManyRequests
 
 export type ServiceErrorStatus =
   | HttpStatus.ServiceUnavailable
@@ -18,6 +19,7 @@ const ERROR_MSG: Record<ClientErrorStatus | ServiceErrorStatus, string> = {
   403: "Forbidden.",
   404: "Not found.",
   405: "Method not allowed.",
+  429: "Too many requests",
   500: "Internal server error.",
   503: "Service unavailable.",
   504: "Service timeout.",
