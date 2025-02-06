9a00d1c069a9143fa8bfd03311fd4c456df21106
Kenneth Keim <32063766+kennethkeim@users.noreply.github.com>
2025-02-06 08:26:52 -0500

    Update demo app email sender

--- packages/demo-ts-app/src/send-error.ts
+++ packages/demo-ts-app/src/send-error.ts
@@ -1,16 +1,12 @@
 import { config } from "dotenv"
 config()
-import { handleApiError, Mailer } from "@kennethkeim/api-utils-core"
+import { emailError, Mailer } from "@kennethkeim/api-utils-core"
 
 const mailer = new Mailer("The Most Awesomest API")
 
-handleApiError(
-  new Error("Oh no"),
-  {
-    event: "test-error-handler",
-    eventType: "oltp",
-    remediation: "there is no hope of recovery",
-    notes: "give up",
-  },
-  mailer
-).then(() => console.log("Sent error email"))
+emailError(new Error("Oh no"), mailer, {
+  event: "test-error-handler",
+  eventType: "oltp",
+  remediation: "there is no hope of recovery",
+  notes: "give up",
+}).then(() => console.log("Sent error email"))
