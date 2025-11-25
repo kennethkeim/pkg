0cf121fedc79e6b7ccb9f49f62b5b0fe9f33cc1c
Kenneth Keim <32063766+kennethkeim@users.noreply.github.com>
2025-11-25 11:35:16 -0500

    Update test emails

--- packages/demo-ts-app/src/send-mail.ts
+++ packages/demo-ts-app/src/send-mail.ts
@@ -7,9 +7,16 @@ const mailer = new Mailer("The Most Awesomest API")
 console.log("Sending mail to", process.env["TEMP_RECIPIENT"])
 mailer.send({
   to: process.env["TEMP_RECIPIENT"],
-  subject: "Hello Friend",
+  subject: "Test Email",
   html: `
     <h1>Hi</h1>
-    <p>This is a test from my mailer app. I hope you like it. The mailer will make it easy to send transactional emails from my APIs. I hope you are doing well today. Please note that whenever I am testing the mailer module, you'll get one of these emails. I hope that's ok. Cheers.</p>
+    <p>Testing Brevo emails...</p>
     `,
+})
+
+console.log("Sending template email to", process.env["TEMP_RECIPIENT"])
+mailer.send({
+  to: process.env["TEMP_RECIPIENT"],
+  templateId: 1,
+  params: { userFirstName: "Ken" },
 })
